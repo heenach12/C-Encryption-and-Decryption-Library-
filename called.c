96c00f70dcebb6f94e16206de61b0a5afeaffedc
Heena <heenachoudhary802@gmail.com>
2017-03-19 22:00:50 +0530

    Add files via upload

---
+++ called.c
@@ -0,0 +1,18 @@
+#include "rsa.h"
+#include "affine.h"
+#include "vignere.h"
+#include "shift.h"
+#include "transpos.h"
+int main()
+{
+//vignere();
+//printf("\n\n");
+//affine();
+//printf("\n\n");
+rsaenc();
+printf("\n\n");
+//printf("\n\n");
+//shift();
+//printf("\n\n");
+//transpos();
+}
